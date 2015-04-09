a199add41f731b50b95409a1d2b24a1fbe17cf7b
Kuod <louismougin54@gmail.com>
2015-04-09 17:54:01 +0200

    modifié:         modele/analyse/AnalyseurLexical.java 	modifié:         modele/analyse/AnalyseurSyntaxique.java 	modifié:         modele/analyse/CodesLexicaux.java 	modifié:         modele/analyse/Grammaire.cup 	nouveau fichier: modele/arbre/MoinsUnaire.java

---
+++ modele/arbre/MoinsUnaire.java
@@ -0,0 +1,22 @@
+package modele.arbre;
+
+public class MoinsUnaire extends Expression {
+
+	protected StringBuilder sb;
+	protected Expression e;
+	public MoinsUnaire(Expression e) {
+		this.e = e;
+		sb = new StringBuilder();
+	}
+	
+	@Override
+	public String getCodeDecore() {
+		sb.append(e.getCodeDecore()+"\n");
+		sb.append("#MoinsUnaire\n");
+		sb.append("neg $v0, $v0\n");
+		return sb.toString();
+	}
+
+
+
+}
