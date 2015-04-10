80eb3cb10f31344fdd989b6825f461304e8347a0
AnthonM <anthony290991@hotmail.com>
2015-04-10 14:09:31 +0200

    Update Booleen.java

--- modele/arbre/logique/Booleen.java
+++ modele/arbre/logique/Booleen.java
@@ -5,18 +5,26 @@ import modele.arbre.Expression;
 public class Booleen extends Expression {
 
 	protected int bool;
-	
+	/**
+	 * Constructeur Booleen
+	 */
 	public Booleen(String val) {
 		if(val.equals("faux")) bool = 0;
 		else bool = 1;
 		isBool = true;
 	}
 	
+	/**
+	 * Retourne un boolean qui vérifie la sémantique
+	 * @return 
+	 */ 
 	public boolean semantiqueCorrect() {
 		return true;
 	}
 	
-	@Override
+	/**
+	 * Retourne le code en MIPS sous forme de chaîne de caractère
+	 */
 	public String getCodeDecore() {
 		StringBuilder sb = new StringBuilder();
 		sb.append("# Booleen "+bool+"\n");
