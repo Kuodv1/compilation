538b967b7ec3d986b67a5ab31ff0d9856aa615a1
Kuod <louismougin54@gmail.com>
2015-05-12 18:02:08 +0200

    ajout de Lire var

--- modele/arbre/Lire.java
+++ modele/arbre/Lire.java
@@ -1,5 +1,11 @@
 package modele.arbre;
 
+import modele.Entree;
+import modele.Symbole;
+import modele.TDS;
+import modele.analyse.exception.IdentifiantInexistantException;
+import modele.analyse.exception.MauvaiseAffectationException;
+
 public class Lire extends Instruction{
 
 	protected String i;
@@ -9,6 +15,27 @@ public class Lire extends Instruction{
 		this.i = i;
 	}
 
+	public String getCodeDecore() {
+		StringBuilder sb = new StringBuilder();
+		
+		Symbole s = TDS.getInstance().identifier(i);
+		int depl = s.getDeplacement();
+		
+		sb.append("#--LIRE entier et stockage dans la var :"+i+"\n");
+		sb.append("li $v0, 5\n");
+		sb.append("syscall\n");
+		sb.append("move $t0, $v0\n");
+		sb.append("sw $t0,"+depl+"($s7)\n");
+		
+		return sb.toString();
+	}
+	
+	public void semantiqueCorrect() throws MauvaiseAffectationException, IdentifiantInexistantException {
+		Entree tmp = new Entree(i,ligne,colonne);
+		if(!TDS.getInstance().contains(tmp)) {
+			throw new IdentifiantInexistantException("Variable "+i+" inexistante.\n",ligne,colonne);
+		}
+	}
 	
 	public String toString() {
 		return "Lire "+i;
